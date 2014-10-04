a26d8be71cd9e35dbd78a5e9034fdb8b7edb25aa
Xenfox <ryandunnison@hotmail.com>
2014-10-03 23:07:40 -0700

    Updated movement with a path. If you leave path, you slow.

--- Assets/_Scripts/MoveDragon.cs
+++ Assets/_Scripts/MoveDragon.cs
@@ -1,36 +1,125 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MoveDragon : MonoBehaviour 
 {
-
-	public float flapSpeed = 1f;
+	public float breath {get; set;}
+	public float flapSpeed = 0.5f;
 	public float moveSpeed = 3f;
+	private bool upDir = false;
+	private bool path = false;
+	private bool center = false;
 
-	public float flyingGravity = 0.5f;
 	public float glidingGravity = -1f;
 
 	public float gravity;
 
+	private string breathState = "Exhale";
 
-
-	// Use this for initialization
 	void Start () 
 	{
+		//Start Character moving forward
 		this.rigidbody2D.velocity = new Vector2(moveSpeed, 0);
 	}
-	
-	// Update is called once per frame
+
 	void Update () 
 	{
-		if(Input.GetKey(KeyCode.Space))
+		//When exhaling, move in paths
+		if(breathState == "Exhale")
 		{
-			this.rigidbody2D.velocity = new Vector2(this.rigidbody2D.velocity.x, this.rigidbody2D.velocity.y + flapSpeed);
+			//Checks if you are in the path
+			if (path)
+			{
+			this.rigidbody2D.velocity = new Vector2(this.rigidbody2D.velocity.x, (breath*10)+flapSpeed);
+			}
+			//If not in path
+			else
+			{
+				//If you breath properly
+				if ((breath > -1)&&(breath < 1))
+				{
+					//If you went below go up
+					if (upDir == false)
+					{
+						this.rigidbody2D.velocity = new Vector2(this.rigidbody2D.velocity.x, (breath*10)+flapSpeed);
+					}
+					//If you went up go down
+					else if (center == false)
+					{
+						this.rigidbody2D.velocity = new Vector2(this.rigidbody2D.velocity.x, -(breath*10)-flapSpeed);
+					}
+					else
+					{
+						this.rigidbody2D.velocity = new Vector2(this.rigidbody2D.velocity.x,0);
+					}
+				}
+				//If you aren't breathing properly don't move higher or lower
+				else
+				{
+					this.rigidbody2D.velocity = new Vector2(this.rigidbody2D.velocity.x, 0);
+				}
+			}
 		}
-		else if(this.rigidbody2D.velocity.y > -10)
+
+		//If you are in a path
+		if (path)
 		{
-			this.rigidbody2D.velocity = new Vector2(this.rigidbody2D.velocity.x, this.rigidbody2D.velocity.y - this.flyingGravity);
+			//If breathing properly move forward properly
+			if ((breath > -1)&&(breath < 1))
+			{
+				this.rigidbody2D.velocity = new Vector2(moveSpeed, this.rigidbody2D.velocity.y);
+			}
+			//Else if breathing too weak slow down
+			else if (breath < -1)
+			{
+				this.rigidbody2D.velocity = new Vector2(moveSpeed/9, this.rigidbody2D.velocity.y);
+			}
+			//Else if breathing too strong slow down
+			else if (breath > 1)
+			{
+				this.rigidbody2D.velocity = new Vector2(moveSpeed/9, this.rigidbody2D.velocity.y);
+			}
+		}
+
+		//When in Inhale, Glide
+		else if (breathState == "Inhale")
+		{
+
 		}
 
 	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "path")
+		{
+			path = true;
+		}
+		if (other.gameObject.tag == "center")
+		{
+			center = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "path")
+		{
+			path = false;
+			if (breath < -1)
+			{
+				upDir = false;
+			}
+			else if (breath > 1)
+			{
+				upDir = true;
+			}
+		}
+		if (other.gameObject.tag == "center")
+		{
+			center = false;
+		}
+	}
+
 }
