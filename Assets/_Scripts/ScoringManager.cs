9f89bb8f5defdafd3a52b1e981eb693ee0f8fb13
Xenfox <ryandunnison@hotmail.com>
2014-10-04 17:43:48 -0700

    Added Flashy Stuff to Feedback

--- Assets/_Scripts/ScoringManager.cs
+++ Assets/_Scripts/ScoringManager.cs
@@ -29,11 +29,52 @@ public class ScoringManager : MonoBehaviour {
 	[SerializeField]
 	private Text feedback;
 
+	private int feedbackUpdate = 0;
+	private Vector2 feedbackStartPos;
+
 	void Start()
 	{
+		feedbackStartPos = feedback.transform.position;
 		feedback.text = "";
 	}
 
+	void UpdateFeedback()
+	{
+		if (feedbackUpdate != 0)
+		{
+		feedback.color = Color.Lerp(feedback.color,new Color(1f,1f,1f,0f),Time.deltaTime);
+		}
+		feedback.transform.position = Vector2.Lerp(feedback.transform.position,new Vector2(feedback.transform.position.x,feedback.transform.position.y +20),Time.deltaTime*5);
+		if ((inputManager.ExhaleCurrent < 1.5f)&&(feedbackUpdate == 0))
+		{
+			feedback.color = new Color(1f,1f,1f,1f);
+			feedback.transform.position = feedbackStartPos;
+			feedback.text = "Good";
+			feedbackUpdate = 1;
+		}
+		else if ((inputManager.ExhaleCurrent < 2.5)&&(feedbackUpdate == 1)&&(inputManager.ExhaleCurrent > 1.5))
+		{
+			feedback.color = new Color(1f,1f,1f,1f);
+			feedback.transform.position = feedbackStartPos;
+			feedback.text = "Great!";
+			feedbackUpdate = 2;
+		}
+		else if ((inputManager.ExhaleCurrent < 3.5)&&(feedbackUpdate == 2)&&(inputManager.ExhaleCurrent > 2.5))
+		{
+			feedback.color = new Color(1f,1f,1f,1f);
+			feedback.transform.position = feedbackStartPos;
+			feedback.text = "Awesome!!!";
+			feedbackUpdate = 3;
+		}
+		else if ((inputManager.ExhaleCurrent < 4)&&(feedbackUpdate == 3)&&(inputManager.ExhaleCurrent > 3.5))
+		{
+			feedback.color = new Color(1f,1f,1f,1f);
+			feedback.transform.position = feedbackStartPos;
+			feedback.text = "AMAZING!!!";
+			feedbackUpdate = 0;
+		}
+	}
+	
 	void Update () 
 	{
 		if (inputManager.ExhaleCurrent >= inputManager.ExhaleTimeMin)
@@ -42,29 +83,34 @@ public class ScoringManager : MonoBehaviour {
 			{
 				IncreaseScore(ScoreType.Good);
 				score.text = "Points: " + currentScore;
-				feedback.text = "Good";
+				//feedback.text = "Good";
+				UpdateFeedback();
 			}
 			else if (inputManager.ExhaleCurrent < 2.5)
 			{
 				IncreaseScore(ScoreType.Great);
 				score.text = "Points: " + currentScore;
-				feedback.text = "Great!";
+				//feedback.text = "Great!";
+				UpdateFeedback();
 			}
 			else if (inputManager.ExhaleCurrent < 3.5)
 			{
 				IncreaseScore(ScoreType.Awesome);
 				score.text = "Points: " + currentScore;
-				feedback.text = "Awesome!!!";
+				//feedback.text = "Awesome!!!";
+				UpdateFeedback();
 			}
 			else if (inputManager.ExhaleCurrent < 4)
 			{
 				IncreaseScore(ScoreType.Amazing);
 				score.text = "Points: " + currentScore;
-				feedback.text = "AMAZING!!!";
+				//feedback.text = "AMAZING!!!";
+				UpdateFeedback();
 			}
 		}
 		else
 		{
+			feedbackUpdate = 0;
 			feedback.text = "";
 		}
 		if(currentlyScoringType != ScoreType.NoScoring)
