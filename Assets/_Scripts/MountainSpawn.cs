e086a2b90455e85d1885f740dc75c1c3651dea1c
Xenfox <ryandunnison@hotmail.com>
2014-10-05 12:30:28 -0700

    Made the mountains tile to some extent. Added the background.

--- Assets/_Scripts/MountainSpawn.cs
+++ Assets/_Scripts/MountainSpawn.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Cloud : MonoBehaviour {
-
+public class MountainSpawn : MonoBehaviour {
+	
 	[SerializeField]
 	private GameObject creator;
-
+	
 	// Use this for initialization
 	void Start () 
 	{
@@ -14,9 +14,9 @@ public class Cloud : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
-
+	
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if(other.name == "GameObjectPoolCollector")
@@ -24,8 +24,8 @@ public class Cloud : MonoBehaviour {
 			if (creator)
 			{
 				//Debug.Log("position" + creator.transform.position.y);
-				float y = Random.Range((creator.transform.position.y - 80/2) - this.renderer.bounds.size.y, 80 + creator.transform.position.y);
-				this.transform.position = new Vector2 (creator.transform.position.x + this.renderer.bounds.size.x, y);
+				//float y = Random.Range((creator.transform.position.y - 80/2) - this.renderer.bounds.size.y, 80 + creator.transform.position.y);
+				this.transform.position = new Vector2 (creator.transform.position.x + (this.renderer.bounds.size.x), transform.position.y);
 			}
 		}
 	}
