40e138d6bd9b5f3ce7c47d2630c6c12905e487de
Xenfox <ryandunnison@hotmail.com>
2014-10-04 20:26:37 -0700

    Revert "Revert "Clouds are now spawning once and then being moved. New jetstream""
    This reverts commit 388252cdcb51be3ebbb86097967e257a67fe197c.

--- Assets/_Scripts/Background/Cloud.cs
+++ Assets/_Scripts/Background/Cloud.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class Cloud : MonoBehaviour {
 
+	[SerializeField]
+	private GameObject creator;
+
 	// Use this for initialization
 	void Start () {
+		creator = GameObject.Find("GameObjectPoolCreator");
 	
 	}
 	
@@ -17,7 +21,12 @@ public class Cloud : MonoBehaviour {
 	{
 		if(other.name == "GameObjectPoolCollector")
 		{
-			GameObject.Destroy(this.gameObject);
+			if (creator)
+			{
+			//GameObject.Destroy(this.gameObject);
+			float y = Random.Range(creator.transform.position.y-this.renderer.bounds.size.y,0);
+				this.transform.position = new Vector2 (creator.transform.position.x + this.renderer.bounds.size.x,y);
+			}
 		}
 	}
 }
