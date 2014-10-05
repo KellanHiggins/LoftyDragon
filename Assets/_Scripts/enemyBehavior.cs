5ae50989c5a9cba85f456084bcab641427ada197
Xenfox <ryandunnison@hotmail.com>
2014-10-05 11:03:20 -0700

    Mini Game beginning

---
+++ Assets/_Scripts/enemyBehavior.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyBehavior : MonoBehaviour {
+	private bool moving = false;
+	[SerializeField]
+	private GameObject enemySpawn1;
+	[SerializeField]
+	private GameObject enemySpawn2;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (moving == false)
+		{
+			int rand = Random.Range(0,2);
+			Debug.Log(rand);
+			if (rand == 0)
+			{
+				this.transform.position = new Vector2(enemySpawn1.transform.position.x,enemySpawn1.transform.position.y);
+				this.rigidbody2D.velocity = new Vector2(-5,0);
+				moving = true;
+			}
+			else
+			{
+				this.transform.position = new Vector2(enemySpawn2.transform.position.x,enemySpawn2.transform.position.y);
+				this.rigidbody2D.velocity = new Vector2(-5,0);
+				moving = true;
+			}
+		}
+	}
+}
